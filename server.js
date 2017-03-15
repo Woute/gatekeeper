a8793700237203a60edbcc6ee14dc653aba560a3
Woote <vivien.faure@wanadoo.fr>
2017-03-15 19:45:43 +0100

    no message

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ function authenticate(body) {
 
 // Convenience for allowing CORS on routes - GET and POST only
 app.all('*', function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'https://woute.github.io/'); 
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); 
+	res.header('Access-Control-Allow-Origin', 'https://woute.github.io/');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
 
 app.post('/authenticate', function(req, res) {
-	console.log('authenticating code:' + req.body.toto);
-	console.log(req.body);
+	console.log('authenticating code:' + JSON.stringify(req.body));
+	console.log(req.get('host' + ' _ ' + req.get('origin') + ' _ ' + req.socket.remoteAddress);
 	authenticate(req.body)
 	.then(result => {
-		res.json({"toto":result});
+		res.raw({"toto":result});
 	});
 });
 
