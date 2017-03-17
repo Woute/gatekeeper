71894ab5a127c2bfe7b4ace0f75ebfc8bda7c52f
Woote <vivien.faure@wanadoo.fr>
2017-03-17 20:25:45 +0100

    no message

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ function evepraisal(body) {
 		rp(options)
 		.then(response => {
 			let result = response.replace('</head>', '<link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">\n    <link href="https://netdna.bootstrapcdn.com/bootswatch/2.3.2/cyborg/bootstrap.min.css" rel="stylesheet">\n    <link href="/static/bootstrap-responsive.min.css" rel="stylesheet">\n</head>');
+			result = result.replace(/<script>[.\s\S]*<\/script>/gm, '');
 			resolve(result);
 		})
 		.catch(err => {
